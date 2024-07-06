646c72d928d22ff9dc8567af8aecbe61e153e042
fateemaazaahraae <fatimazahraebazaz1@gmail.com>
2024-07-06 15:17:07 +0100

    add header file

---
+++ minishell.h
@@ -0,0 +1,21 @@
+#ifndef MINISHELL
+# define MINISHELL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <errno.h>
+#include <signal.h>
+#include <termios.h>
+#include <curses.h>
+#include <term.h>
+
+
+
+#endif
