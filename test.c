8ea85632c5c606682bff516da4f86823d7c889df
AliAkouhar <akouhar04ali@gmail.com>
2024-07-11 10:56:00 +0100

    some changes in parcing

---
+++ test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int count_digit(int nb)
+{
+    int i;
+
+    i = 0;
+    while (nb)
+    {
+        
+    }
+}
+
+int     check_palindrom(int nb)
+{
+    int size;
+
+    size = count_digit(nb);
+}
+
+int main()
+{
+    int nb = 16461;
+    check_palidrom(nb);
+    return (0);
+}
